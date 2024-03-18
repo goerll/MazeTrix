99807dafb7208f520e890f9cb2064daa9f82a626
goerll <estevaogoerlln@gmail.com>
2024-03-17 23:11:00 -0300

    Complete makeover of code structure, creation of the Maze class, fix previous bug

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
+//#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
-#include <iostream>
 #include <random>
 #include <array>
 
@@ -11,10 +11,11 @@ const sf::Color NORD_WHITE(236, 239, 244, 255);
 const sf::Color NORD_BLUE(136, 192, 208, 255);
 
 // Size of stuff
-const int WINDOW_SIZE = 500;
-const int NUM_OF_LINES = 10;
+const int WINDOW_SIZE = 800;
+const int NUM_OF_LINES = 20;
 const int CELL_SIZE = WINDOW_SIZE/NUM_OF_LINES;
 const int WALL_SIZE = 2;
+const int DELAY = 0;
 
 // Stuff
 sf::RectangleShape CELL_SQUARE({CELL_SIZE, CELL_SIZE});
@@ -22,15 +23,26 @@ sf::RectangleShape BORDER_VERTICAL({WALL_SIZE, CELL_SIZE});
 sf::RectangleShape BORDER_HORIZONTAL({CELL_SIZE, WALL_SIZE});
 
 // Color of stuff
-const sf::Color BG_COLOR = sf::Color::Black;
+const sf::Color BG_COLOR = NORD_DARK;
 const sf::Color CELL_COLOR = NORD_DARK;
-const sf::Color BORDER_COLOR = NORD_WHITE;
+const sf::Color BORDER_COLOR = NORD_BLUE;
 const sf::Color HIGHLIGHT_COLOR = NORD_BLUE;
+
+// Random number generation
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dis;
+
+int rand_num(int start, int end) {
+    // Update the distribution range
+    dis.param(std::uniform_int_distribution<>::param_type(start, end));
+    return dis(gen);
+}
 
 class cell{
   public:
     bool active{false};
-    bool highlighted{true};
+    bool highlighted{false};
     bool wall_up{true};
     bool wall_down{true};
     bool wall_left{true};
@@ -67,24 +79,61 @@ class cell{
     }
 };
 
-// Random number generation
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<> dis;
+class Maze{
+  public:
+    std::array<std::array<cell, NUM_OF_LINES>, NUM_OF_LINES> matrix;
 
-int rand_num(int start, int end) {
-    // Update the distribution range
-    dis.param(std::uniform_int_distribution<>::param_type(start, end));
-    return dis(gen);
-}
+    void draw(sf::RenderWindow* window){
+      for(int y = 0; y < NUM_OF_LINES; y++){
+        for(int x = 0; x < NUM_OF_LINES; x++){
+          if(this->matrix[y][x].active){
+            this->matrix[y][x].draw_cell(window, x*CELL_SIZE, y*CELL_SIZE);
+          }
+        }
+      }
+    }
+
+    void mazefy_binary_tree(sf::RenderWindow* window){
+      for(int y = 0; y < NUM_OF_LINES; y++){
+        for(int x = 0; x < NUM_OF_LINES; x++){
+          this->matrix[y][x].active = true;
+
+          int random = rand_num(0, 1);
+
+          if(random == 1 or x == 0 or y == 0){
+            this->matrix[y][x].wall_up = false;
+            if(y > 0){
+              this->matrix[y-1][x].wall_down = false;
+              this->matrix[y-1][x].draw_cell(window, x*CELL_SIZE, (y-1)*CELL_SIZE);
+            }
+          }
+
+          if(random == 0 or x == 0 or y == 0){
+            this->matrix[y][x].wall_left = false;
+            if(x > 0){
+              this->matrix[y][x-1].wall_right = false;
+              this->matrix[y][x-1].draw_cell(window, (x-1)*CELL_SIZE, y*CELL_SIZE);
+            }
+          }
+
+          this->matrix[y][x].draw_cell(window, x*CELL_SIZE, y*CELL_SIZE);
+          window->display();
+          sf::sleep(sf::milliseconds(DELAY));
+        }
+      }
+    }
+
+};
 
 int main(){
     sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE, WINDOW_SIZE), "MazeTrix");
-    window.clear(NORD_WHITE);
-    std::array<std::array<cell, NUM_OF_LINES>, NUM_OF_LINES> matrix;
+
     BORDER_VERTICAL.setFillColor(BORDER_COLOR);
     BORDER_HORIZONTAL.setFillColor(BORDER_COLOR);
-    bool first_run = true;
+
+    Maze maze;
+    maze.mazefy_binary_tree(&window);
+
 
     while (window.isOpen())
     {
@@ -95,56 +144,11 @@ int main(){
                 window.close();
         }
 
-        //window.clear();
+        window.clear(NORD_DARK);
 
-        for(int y = 0; y < NUM_OF_LINES; y++){
-            for(int x = 0; x < NUM_OF_LINES; x++){
-                if(matrix[y][x].active == false){
-                    matrix[y][x].active = true;
-                    int random = rand_num(0, 1);
+        maze.draw(&window);
 
-                    if(random or x == 0 or y ==0){
-                      matrix[y][x].wall_up = false;
-                      if(y > 0){
-                        matrix[y-1][x].wall_down = false;
-                      }
-                    }
-                    if(random == 0 or x == 0 or y == 0){
-                      matrix[y][x].wall_left = false;
-                      if(x > 0){
-                        matrix[y][x-1].wall_right = false;
-                      }
-                    }
-                  }
-            }
-        }
-
-        cell* previous = &matrix[0][0];
-        int previous_x = 0;
-        int previous_y = 0;
-
-        if(first_run){
-            for(int y = 0; y < NUM_OF_LINES; y++){
-                for(int x = 0; x < NUM_OF_LINES; x++){
-                    previous->highlighted = false;
-                    previous->draw_cell(&window, (previous_x)*CELL_SIZE, (previous_y)*CELL_SIZE);
-                    matrix[y][x].draw_cell(&window, x*CELL_SIZE, y*CELL_SIZE);
-
-                    window.display();
-
-                    matrix[y][x].highlighted = false;
-                    previous = &matrix[y][x];
-                    previous_x = x;
-                    previous_y = y;
-                    sf::sleep(sf::milliseconds(30));
-                }
-            }
-            previous->draw_cell(&window, previous_x*CELL_SIZE, previous_y*CELL_SIZE);
-            window.display();
-        }
-        
-        first_run = false;
-
+        window.display();
     }
 
     return 0;
