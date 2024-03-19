fa0eec19261348e853f7e8246fe66240f2b01f31
goerll <estevaogoerlln@gmail.com>
2024-03-18 22:13:59 -0300

    Change compile_run.sh to just compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,2 @@
 g++ -c main.cpp -I "/home/$USER/MazeTrix/SFML-2.6.1/include"
 g++ main.o -o sfml-app -L "/home/$USER/MazeTrix/SFML-2.6.1/lib" -lsfml-graphics -lsfml-window -lsfml-system
-LD_LIBRARY_PATH=/home/$USER/MazeTrix/SFML-2.6.1/lib ./sfml-app
