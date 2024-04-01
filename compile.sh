fa23d41759f3ae27a353f959f85dc2ddeb296ba6
goerll <estevaogoerlln@gmail.com>
2024-04-01 20:01:48 -0300

    Fix script to run on any computer

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 g++ -c main.cpp src/*.cpp -I "./SFML-2.6.1/include"
-g++ *.o -o sfml-app -L "./SFML-2.6.1/lib" -lsfml-graphics -lsfml-window -lsfml-system
+g++ *.o -o sfml-app -L "./SFML-2.6.1/lib" "./SFML-2.6.1/lib/libsfml-graphics.so.2.6" "./SFML-2.6.1/lib/libsfml-window.so.2.6" "./SFML-2.6.1/lib/libsfml-system.so.2.6"
 
