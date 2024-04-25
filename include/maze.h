65a0b046f16bfe50ab27e00aa1e74d094344c7f8
goerll <estevaogoerlln@gmail.com>
2024-04-24 22:44:43 -0300

    Add comments to maze.h

--- include/maze.h
+++ include/maze.h
@@ -9,12 +9,22 @@ public:
     std::array<std::array<Cell, NUM_OF_LINES>, NUM_OF_LINES> matrix;
 
     Maze();
+
+    // Draw the maze on the screen
     void draw(sf::RenderWindow* window);
+    // Reset the matrix's cells to their default state
     void clear();
-    Cell* get_neighbor(Cell*, char direction);
+    // Get a neighbour from a cell
+    Cell* get_neighbor(Cell*, Direction direction);
+    // Get a random unvisited neighbour from a cell
     Cell* random_unvisited_neighbor(Cell* cell);
+    // Verify if a cell is a dead end (has no unvisited neighbours)
     bool is_dead_end(Cell* cell);
+    // Get the cell that is at a specific screen coordinate
     Cell* get_cell(float x, float y);
+    // Turn the matrix into a maze using the binary tree algorithm
     void mazefy_binary_tree(sf::RenderWindow* window);
+    // Turn the matrix into a maze using the depth first serach algorithm
     void mazefy_depth_first_search(sf::RenderWindow* window, Cell* cell);
+
 };
