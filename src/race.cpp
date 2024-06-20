eb9a79e7b5e76fc704fcfeddf60ef70b83f680a7
goerll <estevaogoerlln@gmail.com>
2024-06-20 00:46:27 -0300

    Remove SFML from file

--- src/race.cpp
+++ src/race.cpp
@@ -1,44 +1,59 @@
 #include "../include/race.h"
 #include <SFML/System/Vector2.hpp>
+#include <raylib.h>
 
 Race::Race() {
-    this->mazeGrid[0] = Maze(0, 0);
-    this->mazeGrid[1] = Maze(CELL_SIZE * LINE_NUM, 0);
+    mazeGrid.push_back(Maze({0, 0}));
+    mazeGrid.push_back(Maze({CELL_SIZE * LINE_NUM, 0}));
 }
 
 void Race::draw(){
-    for (Maze maze : mazeGrid) {
+    for (Maze& maze : mazeGrid)
         maze.draw();
-    }
 }
 
 void Race::reset(){
-    for (Maze maze : mazeGrid) {
+    for (Maze& maze : mazeGrid)
         maze.reset();
-    }
 }
 
-Maze* Race::getMaze(Vector2 coord) {
-    if (coord.x < CELL_SIZE * COL_NUM) {
+Maze* Race::getMaze() {
+    Vector2 coord = GetMousePosition();
+    if (coord.x > WIN_SIZE_X || coord.y > WIN_SIZE_Y || coord.x < 0 || coord.y < 0)
+        return nullptr;
+
+    if (coord.x < CELL_SIZE * COL_NUM && coord.y < CELL_SIZE * LINE_NUM)
         return &mazeGrid[0];
-    }
+
     return &mazeGrid[1];
 }
 
-void Race::mazefyDepthFirst(Vector2 coord){
-    Maze* chosenMaze = getMaze(coord);
-    chosenMaze->mazefyDepthFirstSearch(chosenMaze->getCell(coord));
-    for (Maze maze : mazeGrid) {
+void Race::mazefyDepthFirst(){
+    Maze* chosenMaze = getMaze();
+
+    if (chosenMaze == nullptr)
+        return;
+
+    chosenMaze->mazefyDepthFirstSearch(chosenMaze->getMouseCell());
+
+    for (Maze& maze : mazeGrid) {
         if (&maze != chosenMaze) {
-            maze = Maze(*chosenMaze);
+            maze.copyMatrix(*chosenMaze);
         }
     }
 }
 
-void Race::mazefyBinaryTree(Vector2 coord){
-    getMaze(coord)->mazefyBinaryTree();
+void Race::mazefyBinaryTree(){
+    Maze* chosenMaze = getMaze();
 
-    /*for (Maze maze : mazeGrid) {*/
-    /*    maze.matrix = cell_at_mouse->maze->matrix;*/
-    /*}*/
+    if (chosenMaze == nullptr)
+        return;
+
+    chosenMaze->mazefyBinaryTree();
+
+    for (Maze& maze : mazeGrid) {
+        if (&maze != chosenMaze) {
+            maze.copyMatrix(*chosenMaze);
+        }
+    }
 }
