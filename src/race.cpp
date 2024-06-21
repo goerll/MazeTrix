65a47472f5aa02a33c1193f3c694bdf747493f77
goerll <estevaogoerlln@gmail.com>
2024-06-20 23:53:08 -0300

    Fix verification inside of findWay()

--- src/race.cpp
+++ src/race.cpp
@@ -77,43 +77,61 @@ void Race::setPathfinderPosition(){
 
 // Make pathfinder find it's way to cell at mouse (depth first for left maze, binary tree for right maze)
 void Race::findWay() {
+    // Get maze at mouse position
     Maze* chosenMaze = getMaze();
+
+    // If no maze at mouse position, return
     if (chosenMaze == nullptr)
         return;
 
+    // Reset all mazes
     for (auto& maze : mazeGrid){
         maze->resetVisited();
         maze->getPathfinder()->clearPath();
     }
 
+    // Store end position as the cell that's at mouse
     Vector2i end = chosenMaze->getMouseCell();
 
+    // Get pathfinders
     std::unique_ptr<Pathfinder>& depthPathfinder = mazeGrid[0]->getPathfinder();
-    depthPathfinder->pathPush(depthPathfinder->getPosition());
-
     std::unique_ptr<Pathfinder>& breadthPathfinder = mazeGrid[1]->getPathfinder();
+
+    // Add their current position to their path
+    depthPathfinder->pathPush(depthPathfinder->getPosition());
     breadthPathfinder->pathPush(breadthPathfinder->getPosition());
+
+    // Initialize previous list, previous[x][y] contains the cell that was accessed before cell [x][y]
     using std::vector;
     vector<vector<Vector2i>> previous(COL_NUM, vector<Vector2i>(LINE_NUM, {-1, -1}));
 
+    // While pathfinders aren't both at the end
     while (depthPathfinder->getPosition() != end || breadthPathfinder->getPosition() != end) {
 
+        // If depth pathfinder isn't at the end, take one step
         if (depthPathfinder->getPosition() != end) {
+            // Get pathfinder's next step
             Vector2i nextCell = depthPathfinder->getWay();
+            // If there's a next step
             if (nextCell != Vector2i{-1, -1}) {
+                // Pathfinder takes the next step
                 depthPathfinder->setPosition(nextCell);
+                // Increment the cell's times visited
                 mazeGrid[0]->getCell(nextCell).increaseTimesVisited();
+                // Add the step to the path
                 depthPathfinder->pathPush(nextCell);
             }
             else {
+                // If there's no next step, remove the last step from the path
                 depthPathfinder->pathPop();
-                if (depthPathfinder->isPathEmpty()) {
+                // If the path isn't empty
+                if (!depthPathfinder->isPathEmpty()) {
                     depthPathfinder->setPosition(depthPathfinder->getPathTop());
                 }
             }
-
         }
 
+        // If breadth pathfinder isn't at the end, take one step
         if (breadthPathfinder->getPosition() != end) {
             breadthPathfinder->setPosition(end);
         }
